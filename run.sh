ed3b2d5e1c6ba3089934361a1054446b89521445
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-12-23 15:15:54 -0600

    change docker restart policy

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -t relative-value-img . # build image, tag with relative-value-img
-docker run -dit --restart unless-stopped -p 52000:3306 --name relative-value-container relative-value-img # run container from image
+docker run -dit --restart always -p 52000:3306 --name relative-value-container relative-value-img # run container from image
 # if using gdb, need to add `--cap-add=SYS_PTRACE --security-opt seccomp=unconfined` to docker run, see https://stackoverflow.com/a/46676907/2624391
